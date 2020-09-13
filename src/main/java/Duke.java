5b20c4b5dcb91cc5798f3a70f749f2772b52032c
EdmundEXE <edmundexe98@gmail.com>
2020-09-13 19:08:45 +0800

    change else if to switch statements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,140 +7,82 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Duke {
+
+    public static int listCounter = 0;
+    public static ArrayList<Task> myTasks = new ArrayList<>();
+
     public static void main(String[] args) {
-        Scanner UserInput = new Scanner(System.in);
-        int listCounter = 0;
-        ArrayList<Task> myTasks = new ArrayList<>();
-
-
 
         printStartMessage();
+
         try {
+            System.out.println("Here are the tasks on your list:\n");
             readFile("data/tasks.txt");
             System.out.println("____________________________________________________________\n");
         } catch (FileNotFoundException e) {
-            System.out.println("No task.txt found");
+            System.out.println("No task.txt found...creating a text file...\n");
+            File f = new File("data/tasks.txt");        // creating a new file?
+
         }
+        Scanner UserInput = new Scanner(System.in);
 
         while (true) {
-            String command = UserInput.nextLine();  // scan user input
+            String input = UserInput.nextLine();  // scan user input
+            String[] command = input.split(" ",2);
+            System.out.println("____________________________________________________________\n");
             try {
-
-                while (!command.equals("bye")) {     // prog doesnt end unless "bye"
-                    System.out.println("____________________________________________________________\n");
-
-                    if (command.equals("list")) {        // show list
-                        if (listCounter == 0) {         // Error: empty list
-                            throw new EmptyListException();
-                        }
-                        System.out.println("Here are the tasks in your list:");
-                        for (int i = 0; i < listCounter; i++) {
-                            System.out.println((i + 1) + "." + myTasks.get(i));
-                        }
-
-                    } else if (command.startsWith("done")) {       // mark tick
-                        int taskNumber = Integer.parseInt(command.substring(5));
-
-                        if (listCounter == 0) {            // Error: empty list
-                            throw new EmptyListException();
-                        } else if ((taskNumber <= 0) || (taskNumber > (listCounter))) {       // Error: wrong task number
-                            throw new InvalidTaskNumber();
-                        } else {
-                            myTasks.get(taskNumber - 1).markAsDone();
-                            System.out.println("Nice! I've marked this task as done:\n" +
-                                    myTasks.get(taskNumber - 1) +
-                                    "\n");
-                        }
-                    } else if (command.startsWith("todo")) {   // to do command
-                        if (command.length() <= 5) {       // Error: missing details
-                            throw new InsufficientDescriptionException();
-                        }
-                        myTasks.add(new Todo(command.substring(5)));
-                        appendFile("data/tasks.txt", command.substring(5));
-                        System.out.println("Got it. I've added this task: \n" +
-                                myTasks.get(listCounter) +
-                                "\nNow you have " + (listCounter + 1) + " tasks in the list.\n");
-                        listCounter++;
-                    } else if (command.startsWith("deadline")) {       // deadline command
-                        int x = command.indexOf("/by");         // finds index of /by
-
-                        if ((x == -1) || (command.length() <= 9)) {
-                            throw new InsufficientDescriptionException();
-                        } else {
-                            myTasks.add(new Deadline(command.substring(9, x), command.substring(x + 4)));
-                            appendFile("data/tasks.txt", command.substring(9));
-                            System.out.println("Got it. I've added this task: \n" +
-                                    myTasks.get(listCounter) +
-                                    "\nNow you have " + (listCounter + 1) + " tasks in the list.\n");
-                            listCounter++;
-                        }
-
-                    } else if (command.startsWith("event")) {       // event command
-                        int y = command.indexOf("/at");         // finds index of /at
-
-                        if ((y == -1) || (command.length() <= 6)) {
-                            throw new InsufficientDescriptionException();
-                        } else {
-                            myTasks.add(new Event(command.substring(6, y), command.substring(y + 4)));
-                            appendFile("data/tasks.txt", command.substring(6));
-                            System.out.println("Got it. I've added this task: \n" +
-                                    myTasks.get(listCounter) +
-                                    "\nNow you have " + (listCounter + 1) + " tasks in the list.\n");
-                            listCounter++;
-                        }
-
-                    } else if (command.startsWith("delete")) {
-                        int taskNumber = Integer.parseInt(command.substring(7));
-
-                        if (listCounter == 0) {            // Error: empty list
-                            throw new EmptyListException();
-                        } else if ((taskNumber <= 0) || (taskNumber > (listCounter))) {       // Error: wrong task number
-                            throw new InvalidTaskNumber();
-                        } else {
-
-                            System.out.println("Noted! I've removed this task:\n" +
-                                    myTasks.get(taskNumber - 1) +
-                                    "\nNow you have " + (listCounter-1) + " tasks left.");
-                            myTasks.remove(taskNumber - 1);
-                            listCounter--;
-                        }
-                    } else {              // Error Unknown Command
-                        throw new UnknownCommandException();
-                    }
-
-                    System.out.println("____________________________________________________________\n");
-                    command = UserInput.nextLine();
+                switch (command[0]) {
+                case "list":
+                    listCommand();
+                    break;
+                case "done":
+                    int taskNumber = Integer.parseInt(command[1]);
+                    doneCommand(taskNumber);
+                    break;
+                case "todo":
+                    todoCommand(command[1]);
+                    appendFile("data/tasks.txt", command[1]);
+                    break;
+                case "deadline":
+                    deadlineCommand(command[1]);
+                    appendFile("data/tasks.txt", command[1]);
+                    break;
+                case "event":
+                    eventCommand(command[1]);
+                    appendFile("data/tasks.txt", command[1]);
+                    break;
+                case "delete":
+                    taskNumber = Integer.parseInt(command[1]);
+                    deleteCommand(taskNumber);
+                    break;
+                case "bye":
+                    byeCommand();
+                    break;
+                default :
+                    throw new UnknownCommandException();
                 }
-
-                System.out.println("____________________________________________________________\n" +
-                        "Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________");
-
-               break;
-
             } catch (EmptyListException e) {
-                System.out.println("Empty list. Add something!\n" +
-                        "____________________________________________________________\n");
-
+                System.out.println("Empty list. Add something!\n");
 
             } catch (InvalidTaskNumber e) {
-                System.out.println("Invalid Task Number!!!\n" +
-                        "____________________________________________________________\n");
+                System.out.println("Invalid Task Number!!!\n");
 
             } catch (InsufficientDescriptionException e) {
-                System.out.println("Hmmm....You didn't add any details...\n" +
-                        "____________________________________________________________\n");
+                System.out.println("Hmmm....You didn't add any details...\n");
 
             } catch (UnknownCommandException e) {
-                System.out.println("This command was not programmed in me...=O\n" +
-                        "____________________________________________________________\n");
+                System.out.println("This command was not programmed in me...=O\n");
+
             } catch (IOException e) {
-                System.out.println("Error writing to file!\n" +
-                        "____________________________________________________________\n");
+                System.out.println("Error writing to file!\n");
+
+            } finally {
+                System.out.println("____________________________________________________________\n");
             }
 
         }
     }
+
 
     public static void printStartMessage() {
         String logo = " ____        _        \n"
@@ -153,6 +95,93 @@ public class Duke {
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n");
+
+    }
+
+    private static void listCommand() throws EmptyListException {   // show list
+        if (listCounter == 0) {         // Error: empty list
+            throw new EmptyListException();
+        }
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < listCounter; i++) {
+            System.out.println((i + 1) + "." + myTasks.get(i));
+        }
+    }
+
+    private static void doneCommand(int taskNumber) throws EmptyListException,InvalidTaskNumber {    // mark tick
+
+        if (listCounter == 0) {            // Error: empty list
+            throw new EmptyListException();
+        } else if ((taskNumber <= 0) || (taskNumber > (listCounter))) {       // Error: wrong task number
+            throw new InvalidTaskNumber();
+        } else {
+            myTasks.get(taskNumber - 1).markAsDone();
+            System.out.println("Nice! I've marked this task as done:\n" +
+                                myTasks.get(taskNumber - 1) +
+                                "\n");
+        }
+    }
+
+    private static void todoCommand(String description) throws InsufficientDescriptionException {
+
+        if (description.isEmpty()) {
+            throw new InsufficientDescriptionException();
+        }
+        myTasks.add(new Todo(description));
+        System.out.println("Got it. I've added this task: \n" +
+                            myTasks.get(listCounter) +
+                            "\nNow you have " + (listCounter + 1) + " tasks in the list.\n");
+        listCounter++;
+
+    }
+
+    private static void deadlineCommand(String description) throws InsufficientDescriptionException {
+        int x = description.indexOf("/by");         // finds index of /by
+
+        if (x == -1) {          // no /by found
+            throw new InsufficientDescriptionException();
+        } else {
+            myTasks.add(new Deadline(description.substring(0, x), description.substring(x + 4)));
+            System.out.println("Got it. I've added this task: \n" +
+                                myTasks.get(listCounter) +
+                                "\nNow you have " + (listCounter + 1) + " tasks in the list.\n");
+            listCounter++;
+        }
+    }
+
+    private static void eventCommand(String description) throws InsufficientDescriptionException{
+        int y = description.indexOf("/at");         // finds index of /at
+
+        if (y == -1) {
+            throw new InsufficientDescriptionException();
+        } else {
+            myTasks.add(new Event(description.substring(0, y), description.substring(y + 4)));
+            System.out.println("Got it. I've added this task: \n" +
+                                myTasks.get(listCounter) +
+                                "\nNow you have " + (listCounter + 1) + " tasks in the list.\n");
+            listCounter++;
+        }
+    }
+
+    private static void deleteCommand(int taskNumber) throws EmptyListException, InvalidTaskNumber {
+        if (listCounter == 0) {            // Error: empty list
+            throw new EmptyListException();
+        } else if ((taskNumber <= 0) || (taskNumber > (listCounter))) {       // Error: wrong task number
+            throw new InvalidTaskNumber();
+        } else {
+            System.out.println("Noted! I've removed this task:\n" +
+            myTasks.get(taskNumber - 1) +
+            "\nNow you have " + (listCounter-1) + " tasks left.");
+            myTasks.remove(taskNumber - 1);
+            listCounter--;
+        }
+    }
+
+    private static void byeCommand() {
+        System.out.println("____________________________________________________________\n" +
+        "Bye. Hope to see you again soon!\n" +
+        "____________________________________________________________");
+        System.exit(0);
 
     }
 
